9f74c131bd4a6b4c4b3ff96f77c9dfe7c8c6d9cf
Marcus Deans <57301442+marcus-deans@users.noreply.github.com>
2021-11-11 02:21:01 -0500

    Fully commented pinger

--- ICMP_Pinger.py
+++ ICMP_Pinger.py
@@ -44,41 +44,48 @@ def receiveOnePing(mySocket, ID, timeout, destAddr):
         # Fill in start
         # Fetch the ICMP header from the IP packet
         icmpHeader = recPacket[20:28]
+
+        # Go through the header and retrieve the characterizing values that we will examine
         icmpType, icmpCode, myChecksum, packetID, mySequence = struct.unpack('bbHHh', icmpHeader)
 
+        # If the ICMP Type is erroneous, that is, anything but 0 for echo ping response, print error message
         if icmpType != 0:
-            if icmpCode == 0:
-                    return "Destination Network Unreachable"
-            if icmpCode == 1:
-                    return "Destination Host Unreachable"
-            if icmpCode == 2:
-                    return "Destination Protocol Unreachable"
+            if icmpCode == 0:  # ICMP Type 0 and Code 0 case is unreachable destination network
+                return "Destination Network Unreachable"
+            if icmpCode == 1:  # ICMP Type 0 and Code 2 case is unreachable destination host
+                return "Destination Host Unreachable"
+            if icmpCode == 2:  # ICMP Type 0 and Code 3 case is unreachable destination protocol
+                return "Destination Protocol Unreachable"
             return "Other Error: Expected ICMP Type = 0 and Code = 0, but obtained {} and {}".format(icmpType,
-                                                                                                             icmpCode)
-        if packetID != ID:
+                                                                                                     icmpCode)
+            # Any other ICMP Code with Type not 0 fits into 'other errors' category
+
+        if packetID != ID:  # if the packet ID is not as expected, display error
             return 'Expected Packet ID = {}, but obtained {}'.format(ID, packetID)
 
-        bytesAsDouble = struct.calcsize("d")
+        bytesAsDouble = struct.calcsize("d")  # calculate the size of a byte
+        # obtain the time that the packet was sent, by retrieving from struct
         timeSent = struct.unpack("d", recPacket[28:28 + bytesAsDouble])[0]
 
-        rtt = (timeReceived - timeSent) * 1000
-        rtt_count += 1
-        rtt_sum += rtt
-        rtt_min = min(rtt, rtt_min)
-        rtt_max = max(rtt, rtt_max)
+        rtt = (timeReceived - timeSent) * 1000  # RTT is arrival - departure, scaled up
+        rtt_count += 1  # Increment number of packets thus far (for average)
+        rtt_sum += rtt  # Increment sum of RTT thus far (for average)
+        rtt_min = min(rtt, rtt_min)  # Compute the new minimum RTT
+        rtt_max = max(rtt, rtt_max)  # Compute the new maximum RTT
 
-        ipHeader = struct.unpack('!BBHHHBBH4s4s', recPacket[:20])
-        ttl = ipHeader[5]
-        sendAddress = inet_ntoa(ipHeader[8])
-        packetLength = len(recPacket) - 20
+        ipHeader = struct.unpack('!BBHHHBBH4s4s', recPacket[:20])  # retrieve IP header from struct
+        ttl = ipHeader[5]  # TTL is at appropriate field within IP Header
+        sendAddress = inet_ntoa(ipHeader[8])  # Determine the IP address via conversion of field in IP header
+        packetLength = len(recPacket) - 20  # Determine the packet length by subtracting header length
 
         # Explain each line
 
         # Fill in end
-        timeLeft = timeLeft - howLongInSelect
-        if timeLeft <= 0:
+        timeLeft = timeLeft - howLongInSelect # identify new time left having waited for response
+        if timeLeft <= 0: # if remaining time is less than 0, packet timed out so display
             return "Request timed out."
 
+        # return results of ping packet receipt, including most of the fields identified above from struct
         return 'Received {} bytes from {}: icmp_seq={} ttl={} time={:.3f} ms'.format(packetLength, sendAddress,
                                                                                      mySequence, ttl, rtt)
 
@@ -131,6 +138,8 @@ def ping(host, timeout=1):
     # timeout=1 means: If one second goes by without a reply from the server,
     # the client assumes that either the client's ping or the server's pong is lost
     dest = gethostbyname(host)
+    # dest = "128.119.8.148"
+
     print("Pinging " + dest + " using Python:")
     print("")
     # Send ping requests to a server separated by approximately one second
@@ -147,11 +156,14 @@ def ping(host, timeout=1):
             print('{} packets transmitted, {} packets received, {:.1f}% packet loss'.format(count, rtt_count,
                                                                                             packetLossPercentage))
             if rtt_count != 0:
-                print('RTT Min: {:.3f}ms | RTT Avg: {:.3f}ms | RTT Max: {:.3f}ms'.format(rtt_min, rtt_sum / rtt_count, rtt_max))
+                print('RTT Min: {:.3f}ms | RTT Avg: {:.3f}ms | RTT Max: {:.3f}ms'.format(rtt_min, rtt_sum / rtt_count,
+                                                                                         rtt_max))
+
 
 # ping("127.0.0.1")
 # ping("umass.edu")
-ping("alibaba.com")
-# #ping("bbc.com")
-# #ping("unimelb.edu.au")
-# #ping("pretoriazoo.org")
+# ping("alibaba.com")
+# ping("bbc.com")
+# ping("unimelb.edu.au")
+ping("pretoriazoo.org")
+# ping("128.119.8.148")
